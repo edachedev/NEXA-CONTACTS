7171763e0a46945b04e293d6203c6681d6f25858
edachedevops <victor.edache@binghamuni.edu.ng>
2024-08-02 15:16:48 +0100

    python flask contribution

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, redirect, url_for, render_template
+from models import db, Contact
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+@app.before_request
+def create_tables():
+    db.create_all()
+
+@app.route('/')
+def home():
+    contacts = Contact.query.all()
+    return render_template('home.html', contacts=contacts)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_contact():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        phone = request.form.get('phone')
+        email = request.form.get('email')
+        new_contact = Contact(name=name, phone=phone, email=email)
+        db.session.add(new_contact)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('add_contacts.html')
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update_contact(id):
+    contact = Contact.query.get_or_404(id)
+    if request.method == 'POST':
+        contact.name = request.form.get('name')
+        contact.phone = request.form.get('phone')
+        contact.email = request.form.get('email')
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('update_contacts.html', contact=contact)
+
+@app.route('/delete/<int:id>')
+def delete_contact(id):
+    contact = Contact.query.get_or_404(id)
+    db.session.delete(contact)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
